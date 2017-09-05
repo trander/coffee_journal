669bba2ca0c9ca1c6b750e34de32fa9a97e9fe1f
Hyunjung <trlee329@gmail.com>
2017-09-05 17:36:44 +0200

    create chunk

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,16 +4,33 @@ import { Home, About, Posts } from 'pages';
 import Menu from 'components/Menu';
 
 class App extends Component {
+
+    state = {
+        SplitMe: null
+    }
+
+    showSplitMe = () => {
+        import('components/SplitMe').then(({default: Component}) => {
+            this.setState({
+                SplitMe: Component
+            });
+        });
+    }
+
     render() {
+        const { SplitMe } = this.state;
+
         return (
             <div>
                 <Menu/>
+                { SplitMe && <SplitMe/> }
+                <button onClick={this.showSplitMe}>ClickMe</button>
                 <Route exact path="/" component={Home}/>
+                <Route path="/posts" component={Posts}/>
                 <Switch>
                     <Route path="/about/:name" component={About}/>
                     <Route path="/about" component={About}/>
                 </Switch>
-                <Route path="/posts" component={Posts}/>
             </div>
         );
     }
